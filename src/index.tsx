dc8fcf6c6adad5b8ae46b1459bdd1d63ec69e7dc
developer deepu <sudeepsudeep340@gmail.com>
2025-04-06 18:10:46 +0530

    favicon added

--- src/index.tsx
+++ src/index.tsx
@@ -17,7 +17,7 @@ root.render(
     <Helmet
       appTitle="HOME | CIVIC REPORTS"
       favicon={
-        "https://firebasestorage.googleapis.com/v0/b/woid-582b2.appspot.com/o/CIVIC.png?alt=media&token=534afc5b-61d5-4636-9403-3f3ec4a4b439"
+        "https://firebasestorage.googleapis.com/v0/b/woid-582b2.appspot.com/o/CIVIC.png?alt=media&token=3d887a22-c033-4ecb-8fc3-768fdac4f307"
       }
       description="Civic Reports is a dynamic news platform where users can log in, create, and share news stories across various categories, including Local, State, National, World, Business, Stocks, and Politics. Whether you're reporting breaking news, sharing insights, or keeping up with global trends, our platform empowers you to be part of the conversation. With seamless news posting, bookmarking features, and an interactive community, Civic Reports ensures that news is not just consumed but also contributed by the people. Stay informed, share your perspective, and engage with news that matters to you!"
     />
